ebc1b40335da924a64209438dca3ff47b86afefc
erhanatalan <erhanatalan2013@gmail.com>
2023-06-24 11:08:41 +0300

    UP

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-urlpatterns = [
+from .views import UserLoginView, UserLogoutView
 
+urlpatterns = [
+    path('login', UserLoginView.as_view(), name='user_login'),
+    path('logout', UserLogoutView.as_view(), name='user_logout'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+class FixView:
+    template_name = 'user/user_form.html'
+    next_page = 'user_login'
 
-# Create your views here.
+
+# ----------------------------
+# User Login/Logout
+# ----------------------------
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+class UserLoginView(FixView, LoginView):
+    pass
+
+
+class UserLogoutView(FixView, LogoutView):
+    pass
+
+
+# ----------------------------
+# User Registration
+# ----------------------------
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+
+class UserCreateView(FixView,CreateView):
+    form_class = UserCreationForm
+    
